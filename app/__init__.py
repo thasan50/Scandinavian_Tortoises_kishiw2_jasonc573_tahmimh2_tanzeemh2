a99e6406a4eb78f682dccc25514b9734e2531849
jsonano <jchao5768@gmail.com>
2024-11-05 23:22:52 -0500

    Wrote baseline user login and registration program

--- app/__init__.py
+++ app/__init__.py
@@ -6,21 +6,84 @@ P00: Move Slowly and Fix Things
 Time Spent: 0.2
 '''
 
-from flask import Flask, render_template, request, session, redirect
-app = Flask(__name__)
+from flask import Flask, render_template, request, session, redirect, url_for
+import os
 
+app = Flask(__name__)
+app.secret_key = os.urandom(32)
+
+# placeholder example logins (until we have functioning databases)
+logins = { 
+    "jason" : "chao",
+    "kishi" : "wijaya",
+    "tahmim" : "hassan",
+    "tanzeem" : "hasan",
+}
+
+# MAIN PAGE
 @app.route('/', methods=['GET','POST'])
 def home():
-    return render_template('templates/home.html')
+    # print("=====================\n")
+    # print(app)
+    # print("=====================\n")
+    # print(request)
+    # print("=====================\n")
+    # print(request.args)
+    if 'username' in session:
+        return render_template("home.html", logged_in_text="Welcome " + session['name'])
+    else: 
+        return render_template('home.html')
 
+# USER LOGIN
 @app.route('/login', methods=['GET','POST'])
 def login():
-    return "Login & registration goes here"
+    return render_template("login.html")
 
+@app.route('/auth_login', methods=["GET", "POST"])
+def auth_login():
+    if request.method == "POST":
+        # if username and password match in USER DB, then...
+        username = request.form['username']
+        password = request.form['password']
+        if username in logins and logins[username] == password:
+            session['username'] = 'username'
+            session['name'] = username
+            return redirect('/')
+            # return render_template("home.html", logged_in_text="Welcome " + username)
+        else:
+            return render_template("login.html", error_text="Incorrect username or password.")
+
+# USER REGISTRATIONS
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    return "Registration goes here"
+    return render_template("register.html")
 
+@app.route('/auth_reg', methods=["GET", "POST"])
+def auth_reg():
+    if request.method == "POST":
+        new_username = request.form['new_user']
+        new_password = request.form['new_pass']
+        if len(new_username) < 4:
+            return render_template("register.html", error_text="Please provide a new username of at least 4 characters.")
+        elif len(new_password) < 8:
+            return render_template("register.html", error_text="Please provide a new password of at least 8 characters.")
+        else:
+            # ADD USERNAME AND PASSWORD AS NEW ROW IN USER DB
+            logins[new_username] = new_password
+            return render_template("login.html", registered_text="You are now registered! Please log in.")
+    else:
+        return None
+
+# USER LOGOUTS
+@app.route('/logout', methods=["GET", "POST"])
+def logout():
+    session.pop('username', None)
+    session.pop('name', None)
+    return render_template("home.html")
+
+
+
+# STORIES
 @app.route('/view')
 def view():
     return "Viewing stories goes here"
